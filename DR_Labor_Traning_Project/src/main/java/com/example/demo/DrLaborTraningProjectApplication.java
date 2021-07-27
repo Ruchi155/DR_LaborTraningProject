201971e404998008d80ff93a45754e908d6c6dd9
Ruchi155 <patelruchi155@gmail.com>
2021-07-27 11:06:33 -0400

    First commit-  startup project

---
+++ DR_Labor_Traning_Project/src/main/java/com/example/demo/DrLaborTraningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrLaborTraningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrLaborTraningProjectApplication.class, args);
+	}
+
+}
