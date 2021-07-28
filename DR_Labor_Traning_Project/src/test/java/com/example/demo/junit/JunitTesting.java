7f9c2793d882b95ee83929cd24a377e888c6700b
Austin Keiber <adkeiber@gmail.com>
2021-07-28 10:42:00 -0400

    fixing properties

--- DR_Labor_Traning_Project/src/test/java/com/example/demo/junit/JunitTesting.java
+++ DR_Labor_Traning_Project/src/test/java/com/example/demo/junit/JunitTesting.java
@@ -1,5 +1,31 @@
 package com.example.demo.junit;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.DRAPI.repo.JobRepo;
+import com.example.DRAPI.repo.MachineRepo;
+import com.example.DRAPI.repo.TimecardRepo;
+
+@SpringBootTest
 public class JunitTesting {
 
+	@Autowired
+	JobRepo jRepo;
+	
+	@Autowired
+	MachineRepo mRepo;
+	
+	@Autowired
+	TimecardRepo tRepo;
+	
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void jobRepoFindByCodeTest() {
+		
+	}
 }
--- DR_Labor_Traning_Project/src/test/java/com/example/demo/mockito/MockitoTesting.java
+++ DR_Labor_Traning_Project/src/test/java/com/example/demo/mockito/MockitoTesting.java
@@ -1,5 +1,5 @@
 package com.example.demo.mockito;
 
 public class MockitoTesting {
-
+	
 }
