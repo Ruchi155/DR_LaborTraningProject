a98c569f002722bc777d59958919c6c5dfa7cd62
Ruchi155 <patelruchi155@gmail.com>
2021-07-28 16:36:33 -0400

    Admin and Controller thymeleaf

--- DR_Labor_Traning_Project/src/main/java/com/example/DRAPI/service/AdminService.java
+++ DR_Labor_Traning_Project/src/main/java/com/example/DRAPI/service/AdminService.java
@@ -4,5 +4,7 @@ package com.example.DRAPI.service;
 import com.example.DRAPI.model.Admin;
 public interface AdminService
 {
+
+	//Admin get(int id);
 	
 }
--- DR_Labor_Traning_Project/src/main/java/com/example/DRAPI/service/AdminServiceImpl.java
+++ DR_Labor_Traning_Project/src/main/java/com/example/DRAPI/service/AdminServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.DRAPI.model.Admin;
+import com.example.DRAPI.model.Timecard;
 import com.example.DRAPI.repo.AdminRepo;
 
 @Service
@@ -12,7 +13,13 @@ public class AdminServiceImpl implements AdminService
 	@Autowired
 	AdminRepo adminrepo;
 	
+	/*
+	@Override
+	public Admin get(int i) {
+		return adminrepo.findById(i).get();
+	}
 	
+	*/
 }
 
 	
